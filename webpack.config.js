8d4af564bdc87c97c202097211d022e1f25410b2
TatiParra21 <tatiparras21@gmail.com>
2024-07-31 11:21:53 -0500

    ch

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const Dotenv = require('dotenv-webpack');
 
 module.exports ={
     mode: 'development',
@@ -22,5 +23,9 @@ module.exports ={
                 }
             ]
     },
+    plugins: [
+        new Dotenv()
+    ],
+    
     watch:true
 }

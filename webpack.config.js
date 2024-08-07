5a78c4f55d24e4b2b27f379b28ed63afea650910
TatiParra21 <tatiparras21@gmail.com>
2024-08-07 08:35:55 -0500

    pr

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 
 
 module.exports ={
-    mode: 'development',
+    mode: 'production',
     entry:'./src/index.js',
     output: {
         path: path.resolve(__dirname,'dist'),
